761ab807d65579ca63ca651064de4fc2ee35b60d
Alexisonfire95 <vini.marmone@gmail.com>
2025-12-14 18:30:40 +0200

    feat(client): Add IClientRepository methods

--- src/GymManagement.Application/DTOs/CreateMembershipPlanDto.cs
+++ src/GymManagement.Application/DTOs/CreateMembershipPlanDto.cs
@@ -2,7 +2,7 @@
 
 public record CreateMembershipPlanDto
 {
-    public string Name { get; set; }
+    public required string Name { get; set; }
     public int DurationMonth { get; set; }
     public decimal Price { get; set; }
 }
--- src/GymManagement.Infrastructure/Repositories/ClientRepository.cs
+++ src/GymManagement.Infrastructure/Repositories/ClientRepository.cs
@@ -2,12 +2,21 @@
 using GymManagement.Core.Interfaces;
 using GymManagement.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace GymManagement.Infrastructure.Repositories;
 
 public class ClientRepository (GymManagementContext context): IClientRepository
 {
-    public async Task<Client?> GetClientByIdAsync(int clientId)
+    public Task<Client?> GetClientByIdAsync(int clientId)
+    {
+        return GetByIdAsync(clientId);
+    }
+
+    public async Task<Client?> GetByIdAsync(int clientId)
     {
         return await context.Clients
             .Where(c => c.ClientId == clientId)
@@ -17,5 +26,35 @@ public class ClientRepository (GymManagementContext context): IClientRepository
     public async Task AddAsync(Client client)
     {
         await context.Clients.AddAsync(client);
+    }
+
+    public Task<Client?> GetByIdWithEnrollmentsAsync(int clientId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Client>> SearchByNameOrEmailAsync(string searchTerm)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> ExistsWithEmailAsync(string email, int? excludeId = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Client client)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveAsync(Client client)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Client>> ListAsync()
+    {
+        throw new NotImplementedException();
     }
 }
