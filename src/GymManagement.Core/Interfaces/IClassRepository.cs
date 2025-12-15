365c054e1a87545898dec4b7e3baf7ad45c341e4
shdwhte <zinchenko.daria@lll.kpi.ua>
2025-12-15 13:03:13 +0200

    add IClassRepository interface for class management

---
+++ src/GymManagement.Core/Interfaces/IClassRepository.cs
@@ -0,0 +1,18 @@
+using GymManagement.Core.Entities;
+
+namespace GymManagement.Core.Interfaces;
+
+public interface IClassRepository
+{
+    Task<Class?> GetByIdAsync(int id);
+    Task<IEnumerable<Class>> GetAllAsync();
+    Task<Class> CreateAsync(Class classEntity);
+    Task<Class?> UpdateAsync(Class classEntity);
+    Task<bool> DeleteAsync(int id);
+    
+    Task<IEnumerable<Class>> GetScheduleForDateAsync(DateTime date);
+    Task<IEnumerable<Class>> GetScheduleForDateRangeAsync(DateTime startDate, DateTime endDate);
+    Task<bool> HasTimeConflictForCoachAsync(int coachId, DateTime startTime, DateTime endTime, int? excludeClassId = null);
+    Task<IEnumerable<Class>> GetUpcomingClassesByCoachAsync(int coachId);
+    Task<IEnumerable<Class>> GetClassesByCoachAsync(int coachId, DateTime startDate, DateTime endDate);
+}
