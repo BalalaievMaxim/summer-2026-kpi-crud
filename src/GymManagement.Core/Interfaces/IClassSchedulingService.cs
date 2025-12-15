17a47c99bd0b9b0893652caabda9a4f4acdd1283
shdwhte <zinchenko.daria@lll.kpi.ua>
2025-12-15 17:30:18 +0200

    fix: Refactor ClassSchedulingService and add class deletion

--- src/GymManagement.Core/Interfaces/IClassSchedulingService.cs
+++ src/GymManagement.Core/Interfaces/IClassSchedulingService.cs
@@ -6,10 +6,7 @@ namespace GymManagement.Core.Interfaces;
 public interface IClassSchedulingService
 {
     Task<Class> CreateClassAsync(int classTypeId, int coachId, DateTime startTime, DateTime endTime, int capacity);
-    
-    Task<Enrollment> EnrollClientAsync(int clientId, int classId);
-    
-    Task<bool> CancelEnrollmentAsync(int enrollmentId);
+    Task<bool> DeleteClassAsync(int classId);
     
     Task<IEnumerable<Class>> GetScheduleForDateAsync(DateTime date);
     Task<IEnumerable<Class>> GetScheduleForWeekAsync(DateTime startOfWeek);
