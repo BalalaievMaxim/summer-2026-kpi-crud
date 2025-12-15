332ce99c4211f1fc75dadd805ab180048133d1b7
shdwhte <zinchenko.daria@lll.kpi.ua>
2025-12-15 16:36:14 +0200

    feat: add CoachWorkloadDto class for coach workload data

---
+++ src/GymManagement.Core/DTOs/CoachWorkloadDto.cs
@@ -0,0 +1,10 @@
+namespace GymManagement.Core.DTOs;
+
+public class CoachWorkloadDto
+{
+    public int CoachId { get; set; }
+    public string CoachName { get; set; } = string.Empty;
+    public int TotalClassesScheduled { get; set; }
+    public int TotalHoursWorked { get; set; }
+    public int AverageClassSize { get; set; }
+}
