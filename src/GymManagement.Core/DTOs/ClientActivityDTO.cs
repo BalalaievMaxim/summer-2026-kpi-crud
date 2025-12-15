6e118d462d5b45fa0bcc4e6138aac59191eca96d
FlagrateAE <flagrateinc@gmail.com>
2025-12-15 17:22:23 +0200

    feat: DTO for clients OLAP

---
+++ src/GymManagement.Core/DTOs/ClientActivityDTO.cs
@@ -0,0 +1,10 @@
+namespace GymManagement.Core.DTOs;
+
+public class ClientActivityDto
+{
+    public int ClientId { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public int TotalEnrollments { get; set; }
+    public int ClientRank { get; set; } // RANK() OVER window function
+}
